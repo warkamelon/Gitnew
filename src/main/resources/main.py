f0f06a4f5f6956c0ef894c3a89924043cbd2cc04
warkamelon <vikicat126@gmail.com>
2025-11-18 09:52:50 +0300

    код для входа в систему

--- src/main/resources/main.py
+++ src/main/resources/main.py
@@ -55,12 +55,13 @@ CREATE TABLE IF NOT EXISTS Article (
 ''')
 
 
-cursor.execute('INSERT INTO User_role (Name,Comment) VALUES (?, ?)', ('Читатель', 'Чтение и оценка статей'))
-cursor.execute('INSERT INTO User_role (Name,Comment) VALUES (?, ?)', ('Автор', 'Отправление статей'))
-cursor.execute('INSERT INTO User_role (Name,Comment) VALUES (?, ?)', ('Администратор', 'Проверка статей'))
-
-
-conn.commit()
+#cursor.execute('INSERT INTO User_role (Name,Comment) VALUES (?, ?)', ('Читатель', 'Чтение и оценка статей'))
+#cursor.execute('INSERT INTO User_role (Name,Comment) VALUES (?, ?)', ('Автор', 'Отправление статей'))
+#cursor.execute('INSERT INTO User_role (Name,Comment) VALUES (?, ?)', ('Администратор', 'Проверка статей'))
+#cursor.execute('SELECT * FROM User_role')
+#users = cursor.fetchall()
+#print(users)
+#conn.commit()
 
 
 
@@ -77,18 +78,27 @@ def pass_check(passw,name):
     cursor.execute("SELECT Password FROM User WHERE Name = ?", (name,))
     parol = cursor.fetchone()
     if parol and parol[0]== passw:
-        print('Успешно')
+        return True
     else:
-        print("Неверный пароль")
+        print(parol,passw)
+        return False
 #Проверка имени пользователя
 def login(name):
     cursor.execute("SELECT COUNT(*) FROM User WHERE Name = ?", (name,))
     result = cursor.fetchone()
     if result[0]>0:
         password = input('Введите пароль: ')
-        pass_check(password,name)
+        if pass_check(password,name)==True:
+            return True
+        else:
+            quit()
+
     else:
         print("Пользователь не найден")
+        quit()
+
+
+
 
 choice1 = int(input("Выберите действие:\n"
           "1. Создание аккаунта\n"
@@ -100,9 +110,24 @@ if choice1 == 1:
                       '1 - Читатель\n'
                       '2 - Автор\n'
                       '3 - Администратор\n'))
+    if user_role !=1 and user_role!=2 and user_role!=3:
+        print('Неверный ввод')
+        quit()
     add_user(user_name,user_password,user_role)
 elif choice1 == 2:
     name = input("Введите имя пользователя: ")
-    login(name)
+    verif = login(name)
+else:
+    print("Неверный ввод")
+    quit()
 
+if verif ==True:
+    cursor.execute("SELECT User_role_ID FROM User WHERE Name = ?", (name,))
+    role = cursor.fetchone()
+    if role[0] == 1:
+        print('Вы читатель')
+    elif role[0] ==2:
+        print('Вы автор')
+    elif role[0] ==3:
+        print('Вы администратор')
 
